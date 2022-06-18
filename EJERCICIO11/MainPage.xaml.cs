4c044170740554a09dd5c43f7adfb71a796d8d6d
levinlainez <fidelina.aguirre@gmail.com>
2022-06-17 23:24:15 -0600

    Ejercicio11

---
+++ EJERCICIO11/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace EJERCICIO11
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnDividir_Clicked(object sender, EventArgs e)
+        {
+            var num1 = Convert.ToDecimal(txtNum1.Text);
+            var num2 = Convert.ToDecimal(txtNum2.Text);
+            await Navigation.PushAsync(new Views.ResultPage(Convert.ToDecimal(num1 / num2)));
+        }
+
+        private async void btnMultiplicar_Clicked(object sender, EventArgs e)
+        {
+            var num1 = Convert.ToDecimal(txtNum1.Text);
+            var num2 = Convert.ToDecimal(txtNum2.Text);
+            await Navigation.PushAsync(new Views.ResultPage(Convert.ToDecimal(num1 * num2)));
+
+        }
+
+        private async void btnRestar_Clicked(object sender, EventArgs e)
+        {
+            var num1 = Convert.ToDecimal(txtNum1.Text);
+            var num2 = Convert.ToDecimal(txtNum2.Text);
+            await Navigation.PushAsync(new Views.ResultPage(Convert.ToDecimal(num1 - num2)));
+        }
+
+        private async void btnSumar_Clicked(object sender, EventArgs e)
+        {
+            var num1 = Convert.ToDecimal(txtNum1.Text);
+            var num2 = Convert.ToDecimal(txtNum2.Text);
+            await Navigation.PushAsync(new Views.ResultPage(Convert.ToDecimal(num1 + num2)));
+        }
+    }
+}
